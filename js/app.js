ec780610bc46f3d754eb10b93a29bd115bd7a3ac
Hannanah Mobashir <hanana14@yahoo.com>
2017-08-24 18:39:19 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,13 @@
-// VIEW MODEL
+// MODEL
 
-var ViewModel = function() {
+var Cat = function() {
 	var self = this;
-	self.name = ko.observable('Catilicious');
 	self.clickCount = ko.observable(0);
+	self.name = ko.observable('Catilicious');
 	self.imgUrl = ko.observable('https://cdn.pixabay.com/photo/2016/08/10/02/55/kitten-1582384_960_720.jpg');
 	self.nicknames = ko.observable(['Catty', 'Lush', 'Tilly']);
 
-
-	self.incrementCounter = function() {
-		self.clickCount(self.clickCount() + 1);
-	};
-
+	// set cat title based on number of clicks
 	self.title = ko.computed(function() {
 		if(self.clickCount() >= 0 && self.clickCount() <= 5) {
 			return "Newbie"
@@ -33,6 +29,18 @@ var ViewModel = function() {
 		}
 	});
 }
+
+// VIEW MODEL
+
+var ViewModel = function() {
+	var self = this;
+	self.currentCat = ko.observable(new Cat());
+
+	// increase click count for each click
+	self.incrementCounter = function() {
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};
+};
 
 var vm = new ViewModel();
 ko.applyBindings(vm);
